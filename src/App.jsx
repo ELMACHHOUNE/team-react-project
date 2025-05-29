adf690cd93306435a9c17b3ee150a530d9e9f302
Ghada Nasri <nasrighada10@outlook.com>
2025-05-29 21:51:31 +0100

    feat: courses-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar";
+import Courses from "@/components/courses";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Courses />
     </div>
   );
 }
